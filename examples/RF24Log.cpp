2527f3f63164d4bc18dbbc33cef26a7260e48294
Witold Markowski <witold.a.markowski@gmail.com>
2020-10-06 15:59:59 +0100

    Add demo banner.

--- examples/RF24Log.cpp
+++ examples/RF24Log.cpp
@@ -5,26 +5,39 @@
 #include "../src/RF24HardwareSerialLogAppender.h"
 
 // create hardware serial port log appender
-RF24HardwareSerialLogAppender serialLogAppender(&Serial);
+RF24HardwareSerialLogAppender rf24SerialLogAppender(&Serial);
 
 // define global vendor id (it is stored in flash memory)
 const char vendorID[] PROGMEM = "RF24LogExample";
 const char globalProgmemText[] PROGMEM
 = "global PROGMEM message";
 
+// Need to remember that backslash must be escaped in the string text
+// Banner generated with https://devops.datenkollektiv.de/banner.txt/index.html
+const char banner0[] PROGMEM = " ___   ___   ___   _ _    _                 ";
+const char banner1[] PROGMEM = "| _ \\ | __| |_  ) | | |  | |     ___   __ _ ";
+const char banner2[] PROGMEM = "|   / | _|   / /  |_  _| | |__  / _ \\ / _` |";
+const char banner3[] PROGMEM = "|_|_\\ |_|   /___|   |_|  |____| \\___/ \\__, |";
+const char banner4[] PROGMEM = "                                      |___/ ";
+
 void setup()
 {
    // configure serial port baudrate
    Serial.begin(57600);
    // set serial port appender
-   rf24Logger.setAppender(&serialLogAppender);
+   rf24Logger.setAppender(&rf24SerialLogAppender);
 
-   // Info string in flash memory
+   // display banner
    rf24Logger.info((const __FlashStringHelper*) vendorID,
-         F("******************************************"));
-   // Info string in RAM memory
+         (const __FlashStringHelper*) banner0);
    rf24Logger.info((const __FlashStringHelper*) vendorID,
-         "***          Logging example           ***");
+            (const __FlashStringHelper*) banner1);
+   rf24Logger.info((const __FlashStringHelper*) vendorID,
+            (const __FlashStringHelper*) banner2);
+   rf24Logger.info((const __FlashStringHelper*) vendorID,
+            (const __FlashStringHelper*) banner3);
+   rf24Logger.info((const __FlashStringHelper*) vendorID,
+            (const __FlashStringHelper*) banner4);
 }
 
 void logSimpleRamMessage();
