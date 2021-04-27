e49eb63fea7952178696b75427133ae03ea523b9
2bndy5 <2bndy5@users.noreply.github.com>
2021-04-27 05:22:50 +0000

    deploy: 85b7e911de5aa2199ef15adf31d657c9a9e151cb

--- search/all_7.js
+++ search/all_7.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['printflogger_19',['PrintfLogger',['../class_printf_logger.html',1,'PrintfLogger'],['../class_printf_logger.html#a7d1921079221f789a7d94d5a5410b13c',1,'PrintfLogger::PrintfLogger()']]],
+  ['printflogger_19',['PrintfLogger',['../class_printf_logger.html',1,'PrintfLogger'],['../class_printf_logger.html#a5ef609532997ea28cc50fe1ca18f795c',1,'PrintfLogger::PrintfLogger()']]],
   ['printflogger_2ecpp_20',['PrintfLogger.cpp',['../_printf_logger_8cpp.html',1,'']]],
   ['printflogger_2eh_21',['PrintfLogger.h',['../_printf_logger_8h.html',1,'']]]
 ];
--- search/functions_3.js
+++ search/functions_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['printflogger_84',['PrintfLogger',['../class_printf_logger.html#a7d1921079221f789a7d94d5a5410b13c',1,'PrintfLogger']]]
+  ['printflogger_84',['PrintfLogger',['../class_printf_logger.html#a5ef609532997ea28cc50fe1ca18f795c',1,'PrintfLogger']]]
 ];
