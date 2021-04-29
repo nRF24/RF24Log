a3a99dd83ca0bfd9ba28f91816c73fa1935f82a8
2bndy5 <2bndy5@users.noreply.github.com>
2021-04-29 06:23:55 +0000

    deploy: 171568a9a5d6097380e50240f56b48fba7dc5d00

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['howwide_93',['howWide',['../class_r_f24_log_abstract_stream.html#a7c77deb3a80033416e95f7caac00b776',1,'RF24LogAbstractStream']]]
+  ['howwide_93',['howWide',['../class_r_f24_log_abstract_stream.html#a08b13f9222fd4ceb2b6a06c5644a29bd',1,'RF24LogAbstractStream']]]
 ];
