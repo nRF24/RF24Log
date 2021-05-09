52bca3543278a9d19e286a014a5f4e06e71635ad
2bndy5 <2bndy5@users.noreply.github.com>
2021-05-09 01:17:28 +0000

    deploy: 4fc03195d5d9f27d76d67df2a61696976ed70d01

--- search/all_d.js
+++ search/all_d.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['sethandler_79',['setHandler',['../class_r_f24_logging.html#af7fe2277501fdcc0c5731f7d721f7723',1,'RF24Logging']]],
-  ['setloglevel_80',['setLogLevel',['../class_r_f24_dual_log_handler.html#ad81db68291bcdf1fa253ca1fdf700901',1,'RF24DualLogHandler::setLogLevel()'],['../class_r_f24_log_base_handler.html#a65c0c7cee62ba34437e8af1fbfa409b3',1,'RF24LogBaseHandler::setLogLevel()'],['../class_r_f24_log_abstract_handler.html#a964b4de4cae6567127b51502497f55cb',1,'RF24LogAbstractHandler::setLogLevel()']]],
+  ['setloglevel_80',['setLogLevel',['../class_r_f24_dual_log_handler.html#ad81db68291bcdf1fa253ca1fdf700901',1,'RF24DualLogHandler::setLogLevel()'],['../class_r_f24_log_base_handler.html#a05a67bd549dce582b5ac6015f43fd019',1,'RF24LogBaseHandler::setLogLevel()'],['../class_r_f24_log_abstract_handler.html#a964b4de4cae6567127b51502497f55cb',1,'RF24LogAbstractHandler::setLogLevel()']]],
   ['specifier_81',['specifier',['../struct_specifier_parsing.html#a18bfed25f62f0a94faa39100e448739b',1,'SpecifierParsing']]],
   ['specifierformat_2ecpp_82',['SpecifierFormat.cpp',['../_specifier_format_8cpp.html',1,'']]],
   ['specifierformat_2eh_83',['SpecifierFormat.h',['../_specifier_format_8h.html',1,'']]],
