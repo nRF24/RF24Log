94b0e550747ca06a202c44e69dc2b3af6c3755ea
Brendan <2bndy5@gmail.com>
2021-06-03 20:17:10 -0700

    building RPM pkgs also

--- src/cmake/CPackInfo.cmake
+++ src/cmake/CPackInfo.cmake
@@ -9,7 +9,7 @@ if(NOT DEFINED TARGET_ARCH) # TARGET_ARCH is defined only in the toolchain_<ARCH
 endif()
 
 # set the Cpack generators (specific to types of packages to create)
-set(CPACK_GENERATOR DEB) # RPM requires rpmbuild executable
+set(CPACK_GENERATOR DEB RPM) # RPM requires rpmbuild executable
 
 # assemble a debian package filename from known info
 include(InstallRequiredSystemLibraries)
@@ -27,18 +27,32 @@ set(CPACK_DEBIAN_PACKAGE_SECTION libs)
 set(CPACK_DEBIAN_PACKAGE_CONTROL_STRICT_PERMISSION TRUE)
 set(CPACK_DEBIAN_PACKAGE_GENERATE_SHLIBS ON)
 
+###############################
+# info specific rpm (fedora) packages
+###############################
+set(CPACK_RPM_FILE_NAME "lib${LibTargetName}-${${LibName}_VERSION_MAJOR}.${${LibName}_VERSION_MINOR}-${${LibName}_VERSION_PATCH}.${TARGET_ARCH}")
+set(CPACK_RPM_PACKAGE_ARCHITECTURE ${TARGET_ARCH})
+set(CPACK_RPM_PACKAGE_LICENSE "GPLv2.0")
+set(CPACK_RPM_PACKAGE_VENDOR "Humanity")
+
 # create a post-install & post-removal scripts to update linker
 set(POST_SCRIPTS
     ${CMAKE_BINARY_DIR}/DEBIAN/postrm
     ${CMAKE_BINARY_DIR}/DEBIAN/postinst
     )
 foreach(script ${POST_SCRIPTS})
-    file(WRITE ${script} ldconfig)
+    file(WRITE ${script} /sbin/ldconfig)
     execute_process(COMMAND chmod +x ${script})
     execute_process(COMMAND chmod 775 ${script})
 endforeach()
+# declare scripts for deb pkgs
 list(JOIN POST_SCRIPTS ";" EXTRA_CTRL_FILES)
 set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA EXTRA_CTRL_FILES)
+# declare scripts for rpm pkgs
+list(POP_FRONT POST_SCRIPTS CPACK_RPM_POST_UNINSTALL_SCRIPT_FILE)
+list(POP_FRONT POST_SCRIPTS CPACK_RPM_POST_INSTALL_SCRIPT_FILE)
+
 message(STATUS "ready to package: ${CPACK_PACKAGE_FILE_NAME}.deb")
+message(STATUS "ready to package: ${CPACK_RPM_FILE_NAME}.rpm")
 
 include(CPack)
